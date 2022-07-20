597eabe260091842e7cf1b0d790db0c1d721a94d
Octavio Thomsen <63813753+OctavioThomsen@users.noreply.github.com>
2022-07-19 23:01:26 -0300

    Add files via upload

---
+++ ejercicio4/src/ejercicio4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+// Compilacion: gcc -Wall -o ejercicio4 ejercicio4.c
+
+int main(int argc, char *argv[]) {
+	pid_t pid;
+
+	if (!(argc == 2 && (!strncmp(argv[1],"0",1) || atoi(argv[1]) != 0))) {
+		printf("ERROR: SE DEBE INGRESAR UN SOLO ARGUMENTO Y DEBE SER NUMÉRICO\n");
+		return (-1);
+	}
+	
+	// INICIALIZO PROCESO A
+	pid = fork();	
+	if (pid == 0) {
+		char *vector[1];
+		vector[0] = argv[1];
+		vector[1] = NULL;
+		if (execvp("./ProcesoA",vector) == -1) printf("ERROR EN EXECVP PROCESO A\n");
+	}
+		
+	// INICIALIZO PROCESO B
+	pid = fork();	
+	if (pid == 0) {
+		char *vector[1];
+		vector[0] = argv[1];
+		vector[1] = NULL;
+		if (execvp("./ProcesoB",vector) == -1) printf("ERROR EN EXECVP PROCESO B\n");
+	}
+	
+	// INICIALIZO PROCESO C
+	pid = fork();	
+	if (pid == 0) {
+		char *vector[1];
+		vector[0] = argv[1];
+		vector[1] = NULL;
+		if (execvp("./ProcesoC",vector) == -1) printf("ERROR EN EXECVP PROCESO C\n");
+	}
+	
+	while((pid = wait(0)) > 0);
+	
+	return 0;
+}
+
+
---
+++ ejercicio4/src/ejercicio4ProcesoA.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+// Compilacion: gcc -Wall -o ejercicio4ProcesoA ejercicio4ProcesoA.c
+
+#define SEMA "seA"
+#define SEMB "seB"
+#define SEMC "seC"
+
+sem_t *semA;
+sem_t *semB;
+sem_t *semC;
+
+int main(int argc, char **argv) {
+	int cantidad = atoi(argv[0]);
+
+	semA = sem_open(SEMA, O_CREAT, 0644, 1);
+	semB = sem_open(SEMB, O_CREAT, 0644, 0);
+	semC = sem_open(SEMC, O_CREAT, 0644, 0);
+
+	while (cantidad>0) {	
+		cantidad--;
+		sem_wait(semA);
+		printf("A\n");	
+		sem_post(semB);
+		sem_wait(semA);
+		
+		printf("A\n");
+		sem_post(semC);
+	}
+	sem_unlink(SEMA);
+	sem_unlink(SEMB);
+	sem_unlink(SEMC);
+	sem_destroy(semA);
+	sem_destroy(semB);
+	sem_destroy(semC);
+
+	return 0;
+}
+
+
