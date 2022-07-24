c03d3ebc2dd511cec5f2aed812d8160afa8442ea
Jose Zudaire <zudairejosefrancisco@gmail.com>
2022-07-24 14:38:19 -0300

    ejercicio 1 terminado

--- ejercicio1/src/ejercicio1.c
+++ ejercicio1/src/ejercicio1.c
@@ -10,25 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <pthread.h>
 #include <unistd.h>
-//#include <semaphore.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-//#include <sys/shm.h>
 #include <sys/wait.h>
 #include <string.h>
 
 int salir;
 char comando[64];
 pid_t pid;
+char** args;
 
 void f_sigint(int);
 void f_sigchld(int);
+void reemplazar_enter(char*);
+void separar_argumentos(char**, char*);
 
 int main(int argc, char **argv) {
+
+	args = malloc(10*sizeof(char*));
 
 	signal(SIGINT,(void*)f_sigint);
 	signal(SIGCHLD,(void*)f_sigchld);
@@ -37,89 +39,22 @@ int main(int argc, char **argv) {
 	memset(comando, '\0', 64);
 	fgets(comando, 64, stdin);
 
-	for(int i = 0; i < 64; i++) {
-		if(comando[i] == '\n') {
-			comando[i] = '\0';
-		}
-	}
+	reemplazar_enter(comando);
 
 	salir = 1;
 
-	char** args[10];
-
-	for(int j = 0; j < 10; j++) {
-		args[j] = malloc(sizeof(char)*64);
-		memset(args[j], '\0', 64*sizeof(char));
+	for(int k = 0; k < 10; k++) {
+		args[k] = malloc(sizeof(char)*64);
+		memset(args[k], '\0', 64*sizeof(char));
 	}
 
-	int argID = 0;
-	int j = 0;
-
-	for(int i = 0; i < 64; i++) {
-		if(comando[i] == ' ') {
-			//args[argID] = malloc(sizeof(char)*(i-j+1));
-			memcpy(args[argID], &comando[j], (i-j)*sizeof(char));
-			j = i + 1;
-			argID++;
-		}
-	}
-
-	//args[argID] = malloc(sizeof(char)*(64-j));
-	memcpy(args[argID], &comando[j], (64-j));
-	args[argID+1] = NULL;
-
-	/*
-	int argID, j;
-	char** args[10];
-	salir = 1;
-
-	memcpy(comando, "nosalir", 6);*/
+	separar_argumentos(args, comando);
 
 	while((strcmp(comando,"salir")) && (salir==1)) {
 
-		/*argID = 0;
-		j = 0;
-
-		printf("\nEscriba comando a ejecutar: \n");
-		memset(comando, '\0', 64);
-
-		fgets(comando, 64, stdin);
-
-		for(int i = 0; i < 64; i++) {
-			printf("\nEntramos para los enter\n");
-			if(comando[i] == '\n') {
-				comando[i] = '\0';
-			}
-		}
-
-		printf("\nAntes del memset\n");
-
-		for(int i = 0; i < 10; i++) {
-			args[i] = malloc(sizeof(char)*64);
-			memset(args[i], '\0', 64*sizeof(char));
-		}
-
-		printf("\nDespues del memset\n");
-
-		for(int i = 0; i < 64; i++) {
-			if(comando[i] == ' ') {
-				printf("\nEntramos para los espacios\n");
-				//args[argID] = malloc(sizeof(char)*(i-j+1));
-				memcpy(args[argID], &comando[j], (i-j)*sizeof(char));
-				j = i + 1;
-				argID++;
-			}
-		}
-
-		printf("\nDespues de los espacios\n");
-
-		//args[argID] = malloc(sizeof(char)*(64-j));
-		memcpy(args[argID], &comando[j], (64-j));
-		args[argID+1] = NULL;*/
-
 		pid = fork();
 		if(pid == 0) {
-			execlp(args[0], args, NULL);
+			execv(args[0], args);
 			printf("\nComando no válido\n");
 			exit(0);
 		}
@@ -131,33 +66,15 @@ int main(int argc, char **argv) {
 				memset(comando, '\0', 64);
 				fgets(comando, 64, stdin);
 
-				for(int i = 0; i < 64; i++) {
-					if(comando[i] == '\n') {
-						comando[i] = '\0';
-					}
+				reemplazar_enter(comando);
+
+				for(int k = 0; k < 10; k++) {
+					free(args[k]);
+					args[k] = malloc(sizeof(char)*64);
+					memset(args[k], '\0', 64*sizeof(char));
 				}
 
-				for(int j = 0; j < 10; j++) {
-					args[j] = malloc(sizeof(char)*64);
-					memset(args[j], '\0', 64*sizeof(char));
-				}
-
-				argID = 0;
-				j = 0;
-
-				for(int i = 0; i < 64; i++) {
-					if(comando[i] == ' ') {
-						//args[argID] = malloc(sizeof(char)*(i-j+1));
-						memcpy(args[argID], &comando[j], (i-j)*sizeof(char));
-						j = i + 1;
-						argID++;
-					}
-				}
-
-				//args[argID] = malloc(sizeof(char)*(64-j));
-				memcpy(args[argID], &comando[j], (64-j));
-				args[argID+1] = NULL;
-
+				separar_argumentos(args, comando);
 
 			}
 			else {
@@ -167,9 +84,39 @@ int main(int argc, char **argv) {
 	}
 
 	while((pid = wait(0)) > 0);
+	for(int i = 0; i < 10; i++) {
+		free(args[i]);
+	}
+	free(args);
 
 	exit(0);
 
+}
+
+void separar_argumentos(char** args, char* comando) {
+	int argID = 0;
+	int j = 0;
+
+	for(int i = 0; i < 64; i++) {
+		if(comando[i] == ' ') {
+			memcpy(args[argID], &comando[j], (i-j)*sizeof(char));
+			j = i + 1;
+			argID++;
+		}
+	}
+
+	memcpy(args[argID], &comando[j], (64-j));
+	free(args[argID+1]);
+	args[argID+1] = NULL;
+
+}
+
+void reemplazar_enter(char* comando) {
+	for(int i = 0; i < 64; i++) {
+		if(comando[i] == '\n') {
+			comando[i] = '\0';
+		}
+	}
 }
 
 void f_sigint(int s) {
@@ -183,8 +130,13 @@ void f_sigint(int s) {
 	}
 	if(totalNulos == 64) {
 		while((pid = wait(0)) > 0);
+		for(int i = 0; i < 10; i++) {
+			free(args[i]);
+		}
+		free(args);
 		exit(0);
 	}
+
 }
 
 void f_sigchld(int s) {
