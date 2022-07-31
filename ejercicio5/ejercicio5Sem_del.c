551e6ba25529de13173952058b27bf6d7a69a688
Octavio Thomsen <63813753+OctavioThomsen@users.noreply.github.com>
2022-07-31 17:06:34 -0300

    Update ejercicio5Sem_del.c

--- ejercicio5/ejercicio5Sem_del.c
+++ ejercicio5/ejercicio5Sem_del.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <semaphore.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -21,18 +22,28 @@ int main(void) {
 
 	int idSemC = shm_open("/semC", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
 	ftruncate(idSemC, sizeof(sem_t));
+	
+	if (idSemA != (-1) || idSemB != (-1) || idSemC != (-1) ) {
+		printf("\nDestrucción de semáforos con éxito\n");
+		printf("id_Sem_A = %d\n",idSemA);
+		printf("id_Sem_B = %d\n",idSemB);
+		printf("id_Sem_C = %d\n\n",idSemC);
+		semA = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, idSemA, 0);
+		semB = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, idSemB, 0);
+		semC = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, idSemC, 0);
+		
+		sem_destroy(semA);
+		sem_destroy(semB);
+		sem_destroy(semC);
 
-	semA = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, idSemA, 0);
-	semB = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, idSemB, 0);
-	semC = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, idSemC, 0);
+		shmctl(idSemA, IPC_RMID, 0);
+		shmctl(idSemB, IPC_RMID, 0);
+		shmctl(idSemC, IPC_RMID, 0);
 
-	sem_destroy(semA);
-	sem_destroy(semB);
-	sem_destroy(semC);
-
-	shmctl(idSemA, IPC_RMID, 0);
-	shmctl(idSemB, IPC_RMID, 0);
-	shmctl(idSemC, IPC_RMID, 0);
+	} else {
+		printf("Error de semget en la dirección 0xa. Verificar existencia de semáforos\n");
+		return (-1);
+	}
 	
 	exit(0);
 }
