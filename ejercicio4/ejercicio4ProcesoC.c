cb4f19aaf6594baf0990cbe11900d74d06d71b30
Octavio Thomsen <63813753+OctavioThomsen@users.noreply.github.com>
2022-07-31 16:44:06 -0300

    Update ejercicio4ProcesoC.c

--- ejercicio4/ejercicio4ProcesoC.c
+++ ejercicio4/ejercicio4ProcesoC.c
@@ -3,21 +3,18 @@
 #include <semaphore.h>
 #include <fcntl.h>
 
-// Compilacion: gcc -Wall -o ejercicio4ProcesoC ejercicio4ProcesoC.c
+// Compilacion: gcc -Wall -o ejercicio4ProcesoC ejercicio4ProcesoC.c -pthread
 
 #define SEMA "seA"
-#define SEMB "seB"
 #define SEMC "seC"
 
 sem_t *semA;
-sem_t *semB;
 sem_t *semC;
 
 int main(int argc, char **argv) {
 	int cantidad = atoi(argv[0]);
 
 	semA = sem_open(SEMA, O_CREAT, 0644, 1);
-	semB = sem_open(SEMB, O_CREAT, 0644, 0);
 	semC = sem_open(SEMC, O_CREAT, 0644, 0);
 
 	while (cantidad>0) {
